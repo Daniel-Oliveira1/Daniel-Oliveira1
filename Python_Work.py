9c49b2c3e3887f1651857d2c59c6859b4ae4a197
Daniel-Oliveira1 <105179007+Daniel-Oliveira1@users.noreply.github.com>
2022-09-01 18:40:42 -0400

    Update Python_Work.py

--- Python_Work.py
+++ Python_Work.py
@@ -3,18 +3,18 @@
 #Description: This code acts like an pizzaria where you are the customer, you can interact with the code to pruchase items 
 #off the menu and the code will return to you the total.
 
-# print("welcome to my pizzaria, what would you ike today?")
-# print("Menu:")
-# print("Cola: $1.75")
-# print("Pizza slice: $5.00")
-# print("Whole pizza: $250.00")
-# choice=input(" ")
-# if(choice=='Cola'):
-#     quantity=int(input("How much you want you? "))
-#     print("Your total is: $",quantity*1.75)
-# elif(choice=='Cola'):
-#     quantity=int(input("How much you want you? "))
-#     print("Your total is: $",quantity*1.75)
-# elif(choice=='Cola'):
-#     quantity=int(input("How much you want you? "))
-#     print("Your total is: $",quantity*1.75)
+print("welcome to my pizzaria, what would you ike today?")
+print("Menu:")
+print("Cola: $1.75")
+print("Pizza slice: $5.00")
+print("Whole pizza: $250.00")
+choice=input(" ")
+if(choice=='Cola'):
+    quantity=int(input("How much you want you? "))
+    print("Your total is: $",quantity*1.75)
+elif(choice=='Cola'):
+    quantity=int(input("How much you want you? "))
+    print("Your total is: $",quantity*1.75)
+elif(choice=='Cola'):
+    quantity=int(input("How much you want you? "))
+    print("Your total is: $",quantity*1.75)
