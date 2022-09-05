8481168546b2c6946342803768454d0544467462
Daniel-Oliveira1 <105179007+Daniel-Oliveira1@users.noreply.github.com>
2022-09-05 16:15:13 -0400

    Update Light_Sensor_May6.ino

--- Light_Sensor_May6.ino
+++ Light_Sensor_May6.ino
@@ -1,16 +1,18 @@
-int val;
+//Daniel Oliveira
+//May 6th, 2022
+//Description: This code uses arduino to display the values of two light sensors that are connected to the arduino(analog 0 and 1).
+
+int val1;
 int val2;
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 Serial.print("L= ");
-val = analogRead(A0);
+val1 = analogRead(A0);
 val2=analogRead(A1);
-//Serial.println(val);
+Serial.println(val);
 Serial.print("L= ");
 Serial.println(val2);
 delay(10);
